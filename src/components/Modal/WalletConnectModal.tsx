84c6d22b0d115901b637bb4b45321443c0b86b8e
haodev007 <haodev007@gmail.com>
2023-06-14 08:50:39 -0400

    update wallet modal

--- src/components/Modal/WalletConnectModal.tsx
+++ src/components/Modal/WalletConnectModal.tsx
@@ -1,4 +1,4 @@
-import React, { type FC } from "react";
+import React, { type FC, useEffect } from "react";
 import Image from "next/image";
 
 import { FaTimes } from "react-icons/fa";
@@ -28,22 +28,26 @@ const WalletConnectModal: FC<WalletConnectModalProps> = ({ connectWallet }) => {
 
   const duration = 100;
 
+  useEffect(() => {
+    document.documentElement.style.overflow = isWalletModalOpen ? "hidden" : "auto";
+  }, [isWalletModalOpen]);
+
   return (
     <Transition in={isWalletModalOpen} timeout={duration} unmountOnExit>
       {(state) => (
         <Modal
           $state={state}
-          className="transition-opacity duration-100 ease-in-out fixed z-20 top-0 w-full h-full flex items-center bg-[#00000068]"
+          className="transition-opacity overflow-auto duration-100 ease-in-out fixed z-20 top-0 w-full py-10 medium:py-0 h-full flex  bg-[#00000068]"
         >
-          <div className="absolute w-full h-full z-[1]" onClick={closeWalletModal} />
-          <Col className="relative z-[2] mx-auto overflow-auto w-[400px] medium:w-full max-w-full max-h-[80%] my-10 pt-0 font-dm_sans text-white rounded-2xl bg-[#10141f]">
+          <div className="absolute top-0 w-full h-full z-[1]" onClick={closeWalletModal} />
+          <Col className="relative z-[2] m-auto w-[400px] medium:w-full max-w-full h-fit medium:min-h-full font-dm_sans text-white rounded-2xl medium:rounded-none bg-[#10141f]">
             <Row
               action={closeWalletModal}
               className="self-end m-4 p-2 rounded-full bg-gray-600 transition-all cursor-pointer hover:scale-[1.1]"
             >
               <FaTimes />
             </Row>
-            <div className="pb-4">
+            <Col className="pb-4 space-y-4">
               <p className="px-14 pb-12 text-[24px] mobile:text-[20px] text-center font-medium">
                 Connect a wallet on SEI to continue
               </p>
@@ -57,7 +61,7 @@ const WalletConnectModal: FC<WalletConnectModalProps> = ({ connectWallet }) => {
                   <p className="capitalize text-[18px]">{wallet.name}</p>
                 </Row>
               ))}
-            </div>
+            </Col>
           </Col>
         </Modal>
       )}
