f0115429596850e16655414d121fe262f12828db
haodev007 <haodev007@gmail.com>
2023-06-21 10:20:14 -0400

    remove solax banner

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from "next/image";
+// import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
@@ -12,10 +12,10 @@ const Header = () => {
   return (
     <Container>
       <Row className="pt-[14px] pb-[20px] justify-between">
-        <a href="https://solax.so" target="_blank" rel="noreferrer noopener" className="relative flex items-center">
+        {/* <a href="https://solax.so" target="_blank" rel="noreferrer noopener" className="relative flex items-center">
           <Image src="/solax_logo.png" alt="SOLA-X Logo" width={127} height={30} priority />
           <div className="absolute top-0 -left-4 w-[180px] h-[20px] bg-[#6481BB90] blur-[50px]" />
-        </a>
+        </a> */}
         <div className="uppercase space-x-[30px] laptop:space-x-[20px] normal:hidden">
           {MENU_LIST.map((menu, index) => (
             <Link
